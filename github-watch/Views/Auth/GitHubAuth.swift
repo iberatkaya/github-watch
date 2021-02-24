0341833a9fc94457b2e69e7dd437d95faf7728e8
iberatkaya <beratkaya1998@gmail.com>
2021-02-24 13:35:48 +0300

    Updated iOS App views

--- github-watch/Views/Auth/GitHubAuth.swift
+++ github-watch/Views/Auth/GitHubAuth.swift
@@ -3,32 +3,47 @@ import WebView
 
 struct GitHubAuth: View {
     @StateObject var webViewStore = WebViewStore()
-    
+
     @Environment(\.presentationMode) var presentation
     @EnvironmentObject var authInteractor: RealAuthInteractor
     @EnvironmentObject var connectivityController: ConnectivityController
-    
-    var body: some View {
-        NavigationView {
-            WebView(webView: webViewStore.webView)
-                .navigationBarTitle(Text(verbatim: webViewStore.url?.absoluteString ?? ""), displayMode: .inline)
-        }.onAppear {
-            self.webViewStore.webView.load(URLRequest(url: URL(string: "https://github.com/login/oauth/authorize?scope=read:user%20repo&client_id=\(clientID)&allow_signup=false&state=abcde")!))
-        }.onChange(of: webViewStore.url, perform: { value in
-            if(value != nil && value!.queryParameters?["code"] != nil){
-                guard let code = value!.queryParameters!["code"] else {
-                    return
-                }
-                
-                authInteractor.requestAccessToken(code: code, completed: { accessToken in
-                    if let accessToken = accessToken {
-                        connectivityController.sendOAuthTokenToWatch(token: accessToken)
-                    }
-                    DispatchQueue.main.async {
-                        presentation.wrappedValue.dismiss()
-                    }
-                })
+
+    init() {
+        print("clear cache")
+        URLCache.shared.removeAllCachedResponses()
+
+        if let cookies = HTTPCookieStorage.shared.cookies {
+            for cookie in cookies {
+                print("\(cookie)")
             }
-        })
+        }
+
+        let storage = HTTPCookieStorage.shared
+        for cookie in storage.cookies! {
+            storage.deleteCookie(cookie)
+        }
+    }
+
+    var body: some View {
+        WebView(webView: webViewStore.webView)
+            .navigationBarTitle(Text(verbatim: webViewStore.url?.absoluteString ?? ""), displayMode: .inline)
+            .onAppear {
+                self.webViewStore.webView.load(URLRequest(url: URL(string: "https://github.com/login/oauth/authorize?scope=read:user%20repo&client_id=\(clientID)&allow_signup=false&state=abcde")!))
+            }.onChange(of: webViewStore.url, perform: { value in
+                if value != nil, value!.queryParameters?["code"] != nil {
+                    guard let code = value!.queryParameters!["code"] else {
+                        return
+                    }
+
+                    authInteractor.requestAccessToken(code: code, completed: { accessToken in
+                        if let accessToken = accessToken {
+                            connectivityController.sendOAuthTokenToWatch(token: accessToken)
+                        }
+                        DispatchQueue.main.async {
+                            presentation.wrappedValue.dismiss()
+                        }
+                    })
+                }
+            })
     }
 }
--- github-watch/Views/Home/Home.swift
+++ github-watch/Views/Home/Home.swift
@@ -4,21 +4,54 @@ struct Home: View {
     @EnvironmentObject var authInteractor: RealAuthInteractor
     @EnvironmentObject var appState: AppState
     @EnvironmentObject var connectivityController: ConnectivityController
-    
+
     var body: some View {
         ScrollView {
-            if(appState.user.accessToken == nil){
+            if appState.user.accessToken == nil {
+                Image("Logo")
+                Text("Welcome to Git Watch")
+                    .font(.system(size: 20))
+                    .foregroundColor(.gray)
+                    .bold()
+                    .padding(.bottom, 16)
                 NavigationLink(destination: GitHubAuth()
-                                .environmentObject(authInteractor)
-                                .environmentObject(connectivityController)){
-                    Text("Login To GitHub!").bold()
+                    .environmentObject(authInteractor)
+                    .environmentObject(connectivityController)) {
+                    Text("Login To GitHub!")
+                        .font(.system(size: 16))
+                        .bold()
+                        .padding(.vertical, 12)
+                        .padding(.horizontal, 16)
+                        .background(RoundedRectangle(cornerRadius: 16, style: .continuous).fill(Color.blue.opacity(0.1)))
+                        .foregroundColor(.blue)
                 }
             } else {
-                Text("You are logged in")
-                    .padding()
-                Button("Sign Out", action: {
+                Image("Logo")
+                Text("Welcome to Git Watch")
+                    .font(.system(size: 20))
+                    .foregroundColor(.gray)
+                    .bold()
+                    .padding(.bottom, 16)
+                HStack {
+                    Text("You are logged in!")
+                        .font(.system(size: 16))
+                    Image(systemName: "checkmark.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.blue)
+                        .frame(width: 16, height: 16)
+                }.padding(.bottom, 4)
+                Button(action: {
                     authInteractor.signOut()
-                })
+                }) {
+                    Text("Sign Out")
+                    .font(.system(size: 16))
+                    .bold()
+                    .padding(.vertical, 12)
+                    .padding(.horizontal, 16)
+                    .background(RoundedRectangle(cornerRadius: 16, style: .continuous).fill(Color.blue.opacity(0.1)))
+                    .foregroundColor(.blue)
+                }
             }
         }
     }
@@ -26,6 +59,6 @@ struct Home: View {
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
-        Home().environmentObject(AppState())
+        Home().environmentObject(AppState()).environmentObject(RealAuthInteractor(appState: AppState())).environmentObject(ConnectivityController())
     }
 }
