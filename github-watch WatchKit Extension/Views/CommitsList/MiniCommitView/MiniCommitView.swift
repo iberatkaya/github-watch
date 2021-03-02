7d3e8d8b7bba96117a711971668a11013572899d
iberatkaya <beratkaya1998@gmail.com>
2021-03-02 10:47:53 +0300

    Updated commit style

--- github-watch WatchKit Extension/Views/CommitsList/MiniCommitView/MiniCommitView.swift
+++ github-watch WatchKit Extension/Views/CommitsList/MiniCommitView/MiniCommitView.swift
@@ -1,38 +1,39 @@
-import SwiftUI
 import DateHelper
+import SwiftUI
 struct MiniCommitView: View {
     let commit: Commit
-    
+
     var body: some View {
-        VStack{
+        VStack {
             Text(commit.message ?? "")
                 .bold()
-                .font(.system(size: 14))
+                .font(.system(size: 13))
                 .foregroundColor(.white)
                 .padding(.bottom, 4)
-            Text(commit.date?.toStringWithRelativeTime() ?? "")
-                .font(.system(size: 13))
-                .foregroundColor(.gray)
-                .padding(.bottom, 4)
-            HStack{
+            HStack {
+                Spacer()
+                Text(commit.date?.toStringWithRelativeTime() ?? "")
+                    .font(.system(size: 9))
+                    .foregroundColor(.gray)
+                    .padding(.bottom, 4)
+            }
+            HStack {
                 if let avatar = commit.owner?.avatarUrl {
-                    UserImage(url: avatar, width: 28, height: 28).padding(.trailing, 4)
+                    UserImage(url: avatar, width: 25, height: 25).padding(.trailing, 4)
                 }
-                
+
                 Text(commit.owner?.username ?? "")
                     .font(.system(size: 12))
                     .foregroundColor(.gray)
                     .padding(.leading, 4)
                 Spacer()
             }
-            
-
-        }
+        }.padding(.horizontal, 4)
     }
 }
 
 struct MiniCommitView_Previews: PreviewProvider {
     static var previews: some View {
-        MiniCommitView(commit: Commit(message: "Initial Commit", owner: ProfileUser(username:"stelselim",avatarUrl: "https://yt3.ggpht.com/ytc/AAUvwnioHoNurvLcjTKv2ZVhV8Nn5jroh9At5HZixuMxhA=s176-c-k-c0x00ffffff-no-rj"), date: Date()))
+        MiniCommitView(commit: Commit(message: "Initial Commit", owner: ProfileUser(username: "stelselim", avatarUrl: "https://yt3.ggpht.com/ytc/AAUvwnioHoNurvLcjTKv2ZVhV8Nn5jroh9At5HZixuMxhA=s176-c-k-c0x00ffffff-no-rj"), date: Date()))
     }
 }
